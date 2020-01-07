7371a6bf5ba249980d920a09e489d56939166865
Pablo Romo <pabsromo@gmail.com>
2020-01-07 09:04:06 -0600

    updating spa1.js to fix addEventListener loading error

--- spa1.js
+++ spa1.js
@@ -1,5 +1,7 @@
 let time = document.getElementById('time');
 
-document.getElementById('start').addEventListener('click', function() {
-  time.innerHTML = 'working!';
-});
+if(time) {
+    document.getElementById('start').addEventListener('click', function() {
+      time.innerHTML = 'working!';
+  });
+}
