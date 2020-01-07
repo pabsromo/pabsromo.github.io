e7e52019e594579236e747b6c3d3921f1ed46546
Pablo Romo <pabsromo@gmail.com>
2020-01-07 09:17:09 -0600

    fixed logic for timer to restart correctly

--- spa1.js
+++ spa1.js
@@ -1,5 +1,26 @@
 let time = document.getElementById('time');
+let timeCount = 0;
+let timer;
+let goTime = true;
 
 document.getElementById('start').addEventListener('click', function() {
-  time.innerHTML = 'working!';
+  if(goTime) {
+    goTime = false;
+    timer = setInterval(function() {
+      timeCount++;
+      time.innerHTML = timeCount;
+    }, 1000);
+  }
+});
+
+document.getElementById('stop').addEventListener('click', function() {
+  goTime = true;
+  clearInterval(timer);
+});
+
+document.getElementById('restart').addEventListener('click', function() {
+  goTime = true;
+  clearInterval(timer);
+  timeCount = 0;
+  time.innerHTML = timeCount;
 });
